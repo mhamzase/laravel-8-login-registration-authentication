81ccef896d5c12ba0939d5a9701686f306bc8b3b
mhamzase <mhamzasulehri143@gmail.com>
2021-06-21 08:50:57 +0500

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// this route for getting dashboard view
+Route::get('dashboard', [AuthController::class, 'dashboard']);
+
+// these routes for getting login page and login post 
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('post-login', [CustomAuthController::class, 'customLogin'])->name('login.post'); 
+
+
+// these routes for getting register page and register post 
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('post-registration', [CustomAuthController::class, 'customRegistration'])->name('register.post'); 
+
+// this route for logout user
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+
